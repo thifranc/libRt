f1ef94a966812dd1ba5ceace203a1170be6c21d0
Thibault FRANCOIS <thifranc@e2r6p6.42.fr>
2016-03-13 15:07:53 +0100

    add memmove + del a.out

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: thifranc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/16 08:32:45 by thifranc          #+#    #+#             */
-/*   Updated: 2016/02/20 16:29:55 by thifranc         ###   ########.fr       */
+/*   Updated: 2016/03/12 16:40:31 by thifranc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 		return (NULL);
 	ft_memcpy(out, src, len);
 	ft_memcpy(dst, out, len);
-	free(out);
+	ft_memdel((void**)&out);
 	return (dst);
 }
