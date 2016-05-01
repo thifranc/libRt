f3b08fb6761cc6c2221f9b22abb63c9079709216
Thibault FRANCOIS <thifranc@e3r4p1.42.fr>
2016-05-01 15:03:20 +0200

    improved

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: thifranc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/16 09:40:39 by thifranc          #+#    #+#             */
-/*   Updated: 2016/02/20 16:28:00 by thifranc         ###   ########.fr       */
+/*   Updated: 2016/05/01 13:48:48 by thifranc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 void	ft_putendl_fd(char const *s, int fd)
 {
-	int i;
+	char	*out;
+	int		len;
 
-	i = 0;
-	while (s[i])
-	{
-		write(fd, &s[i], 1);
-		i++;
-	}
-	write(fd, "\n", 1);
+	len = ft_strlen(s);
+	if (!(out = (char*)malloc(sizeof(char) * (len + 2))))
+		return ;
+	ft_strcpy(out, s);
+	out[len] = '\n';
+	out[len + 1] = '\0';
+	write(fd, out, len + 2);
+	ft_memdel((void**)&out);
 }
